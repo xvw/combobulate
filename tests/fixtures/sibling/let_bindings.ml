052b29592d23965ba7d1c6c69b005ac43288a783
PizieDust <playersrebirth@gmail.com>
2025-09-22 02:32:34 +0100

    let bindings test fixtures

---
+++ tests/fixtures/sibling/let_bindings.ml
@@ -0,0 +1,21 @@
+let simple_integer = 42
+
+let int32_literal = 42l
+
+let int64_literal = 42L
+
+let nativeint_literal = 42n
+
+let integer_with_suffix = 42g 
+
+let char_literal = 'a'
+
+let string_literal = "hello world"
+
+let quoted_string_literal = {|A string with "quotes" inside it|}
+
+let float_literal = 3.14159
+
+let scientific_notation_float = 6.022e23
+
+let float_with_suffix = 1.0g 
