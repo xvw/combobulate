a64054e9c9ac9d189532bd89dda2be540f226368
PizieDust <playersrebirth@gmail.com>
2025-09-22 03:14:06 +0100

    nested type declarations test fixture

---
+++ tests/fixtures/down/type_declarations.ml
@@ -0,0 +1,10 @@
+type second_level_page =
+  | Regular of {
+      header : unwind_info_section_header;
+      entries : unwind_info_regular_second_level_entry array;
+    }
+  | Compressed of {
+      header : unwind_info_compressed_section_header;
+      encoding_array : compact_unwind_encoding array;
+      entries : unwind_info_compressed_second_level_entry array;
+    }
